e27bc2c824260619769690c026d97b7b47205e58
Christopher Bero <bigbero@gmail.com>
2017-02-20 22:12:59 +0000

    Systemd service file to automatically start the video stream

--- video/stream.sh
+++ video/stream.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-set -xe
-mjpg_streamer -i "input_uvc.so -d /dev/video0 -n -f 10 -r 640x480" -o "output_http.so -p 8080 -w /tmp/www"
+
+# Force script to fail if any component command fails
+set -e
+
+# There can only be one camera.
+CAMERA=("/dev/video"*)
+echo "Video device: ${CAMERA[0]}"
+
+mjpg_streamer -i "input_uvc.so -d ${CAMERA[0]} -n -f 10 -r 640x480" -o "output_http.so -p 8080 -w /tmp/www"
